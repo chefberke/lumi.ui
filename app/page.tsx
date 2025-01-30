551c59fe0752ae728ef33818a62d88f9f3ca41db
Harun <spadeycontent@gmail.com>
2025-01-30 18:26:28 +0300

    bug-fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
       <div className="absolute inset-0 -z-1 h-full w-full mx-auto bg-[linear-gradient(to_right,#1F1F1F_1px,transparent_1px),linear-gradient(to_bottom,#1F1F1F_1px,transparent_1px)] bg-[size:6rem_4rem]">
         <section className="w-full h-full max-w-full container px-4 py-16 sm:py-20 md:py-24 flex flex-col items-center justify-center text-center">
           <div className="flex flex-col justify-center space-y-4 text-center z-10 w-full max-w-5xl">
-            <div className="space-y-2">
+            <div className="space-y-2 mt-24">
               <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-foreground to-foreground/50">
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400">
                   LumiUI{" "}
